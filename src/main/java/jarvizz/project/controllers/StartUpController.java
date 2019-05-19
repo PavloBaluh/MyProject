25ec53cc333d4705ee4cdacd9e8e95cdf64a056d
PavloBaluh <BaluhPavlo@gmail.comgit config --global user.email baluhpavlo@gmail.com>
2019-05-19 23:33:12 +0300

    end

--- src/main/java/jarvizz/project/controllers/StartUpController.java
+++ src/main/java/jarvizz/project/controllers/StartUpController.java
@@ -13,6 +13,7 @@ public class StartUpController {
     UserService userService;
     @GetMapping("/")
     public String home(){
+        System.out.println("helll");
         return "forward:/index.html";
     }
 
